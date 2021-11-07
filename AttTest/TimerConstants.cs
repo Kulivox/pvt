e45a241d94e38253a1657f6a585cb50a473b6b3c
Michal Klinka <mich.klinka@gmail.com>
2021-11-07 20:27:24 +0100

    Created prototype

---
+++ AttTest/TimerConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace AttTest
+{
+    public class TimerConstants
+    {
+        public int RoundLengthMinSeconds { get; set; }
+        
+        public int RoundLengthMaxSeconds { get; set; }
+        
+        public int FocusPointVisibilityLength { get; set; }
+
+        public int TestLengthSeconds { get; set; }
+
+        public int IntroLengthSeconds { get; set; }
+    }
+}
---
+++ AttTest/TimerConstantsProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace AttTest
+{
+    public class TimerConstantsProvider
+    {
+        public static TimerConstants? GetTimerConstants(string path)
+        {
+            var conf = File.ReadAllText(path);
+            try
+            {
+                return JsonSerializer.Deserialize<TimerConstants>(conf);
+            }
+            catch (JsonException e)
+            {
+                return null;
+            }
+        }
+    }
+}
